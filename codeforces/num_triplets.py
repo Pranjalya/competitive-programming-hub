159bdd162bcdacb673046c003594df2e6858d037
Pranjalya <pranjalyawarrior@gmail.com>
2020-12-15 10:34:58 +0530

    codeforces added

--- codeforces/num_triplets.py
+++ codeforces/num_triplets.py
@@ -8,7 +8,7 @@ Output: 1
 Explanation: Type 1: (1,1,2), nums1[1]^2 = nums2[1] * nums2[2]. (4^2 = 2 * 8). 
 
 '''
-
+import collections
 
 class Solution:
     def numTriplets(self, nums1: List[int], nums2: List[int]) -> int:
@@ -17,12 +17,12 @@ class Solution:
         ans = 0
         for i in nums1:
             g1[i**2] += 1
-         for i in nums2:
+        for i in nums2:
             g2[i**2] += 1
-         for i in range(len(nums1)-1):
+        for i in range(len(nums1)-1):
             for j in range(i+1, len(nums1)):
                 ans += g2[nums1[i]*nums1[j]]
-         for i in range(len(nums2)-1):
+        for i in range(len(nums2)-1):
             for j in range(i+1, len(nums2)):
                 ans += g1[nums2[i]*nums2[j]]
-        return res
+        return ans
